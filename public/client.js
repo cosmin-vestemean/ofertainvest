2c405b5926466282502c4da6e9a04867f1f5ee12
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-02-16 17:59:57 +0200

    Update saveOferta function UI

--- public/client.js
+++ public/client.js
@@ -892,6 +892,15 @@ async function saveOferta() {
     return
   }
 
+  var hourglassIcon =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hourglass-split" viewBox="0 0 16 16">' +
+    '<path d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z"/>' +
+    '</svg>'
+  var btn_oferta = document.getElementById('btn_oferta')
+  btn_oferta.innerHTML = hourglassIcon + ' Salvare...'
+  //bg-warning
+  btn_oferta.classList.remove('btn-success')
+  btn_oferta.classList.add('btn-warning')
   //exec getValFromQuery and get findoc from result.value
   var findoc = 0
   await connectToS1Service().then(async (result) => {
@@ -974,14 +983,12 @@ async function saveOferta() {
         .then((result) => {
           console.log('result', result)
           if (result.success) {
-            var btn_oferta = document.getElementById('btn_oferta')
             btn_oferta.innerHTML = 'Oferta salvata cu findoc=' + result.id
             btn_oferta.classList.remove('btn-danger')
             btn_oferta.classList.add('btn-success')
             //disable button
             btn_oferta.disabled = true
           } else {
-            var btn_oferta = document.getElementById('btn_oferta')
             btn_oferta.innerHTML = 'Eroare'
             btn_oferta.classList.remove('btn-success')
             btn_oferta.classList.add('btn-danger')
