d33036a3e1234b431247a60b8b541b79e97c9414
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-02-16 17:49:24 +0200

    Update client.js with download button and disable save button after successful save

--- public/client.js
+++ public/client.js
@@ -99,7 +99,12 @@ function loadDataFromFile(evt) {
   reader.readAsBinaryString(file)
 
   var btn_oferta = document.getElementById('btn_oferta')
-  btn_oferta.innerHTML = 'Salveaza oferta initiala'
+  var dl =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">' +
+    '<path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>' +
+    '<path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>' +
+    '</svg>'
+  btn_oferta.innerHTML = 'Salveaza oferta initiala ' + dl
   btn_oferta.classList.remove('btn-success')
   btn_oferta.classList.add('btn-danger')
 }
@@ -973,6 +978,8 @@ async function saveOferta() {
             btn_oferta.innerHTML = 'Oferta salvata cu findoc=' + result.id
             btn_oferta.classList.remove('btn-danger')
             btn_oferta.classList.add('btn-success')
+            //disable button
+            btn_oferta.disabled = true
           } else {
             var btn_oferta = document.getElementById('btn_oferta')
             btn_oferta.innerHTML = 'Eroare'
