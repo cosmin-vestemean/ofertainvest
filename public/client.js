21fb24b980df84c63725e654122263fc289bd28c
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-04-01 09:55:02 +0300

    Update button styles in client.js

--- public/client.js
+++ public/client.js
@@ -651,7 +651,7 @@ function pushDataToTable(data, thead_name, tbody_name) {
         var button = document.createElement('button')
         button.type = 'button'
         button.classList.add('btn')
-        button.classList.add('btn-primary')
+        button.classList.add('btn-outline-primary')
         button.classList.add('btn-sm')
         //add filter icon
         var filter_icon = document.createElement('i')
@@ -680,7 +680,7 @@ function pushDataToTable(data, thead_name, tbody_name) {
         var button = document.createElement('button')
         button.type = 'button'
         button.classList.add('btn')
-        button.classList.add('btn-info')
+        button.classList.add('btn-outline-info')
         button.classList.add('btn-sm')
         //add list icon
         var list_icon = document.createElement('i')
