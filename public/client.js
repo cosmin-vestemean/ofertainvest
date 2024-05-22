4012c616d950bcd2914d23f588337ec23e02c192
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-05-22 16:58:24 +0300

    Refactor client.js to include old_WBS property for related objects in reteta array

--- public/client.js
+++ public/client.js
@@ -1454,11 +1454,11 @@ export function init() {
         for (let i = 0; i < children.length; i++) {
           let child = children[i]
           let obj2 = {}
-          if (child.old_WBS) {
+          if (child.object.old_WBS) {
             //primul termen strikethrough si al doilea termen normal
-            obj2.old_WBS = '<span style="text-decoration: line-through;">' + child.old_WBS + '</span>'
+            obj2.object.old_WBS = '<span style="text-decoration: line-through;">' + child.object.old_WBS + '</span>'
           } else {
-            obj2.old_WBS = ''
+            obj2.object.old_WBS = ''
           }
           obj2.WBS = child.object.WBS
           obj2.DENUMIRE_ARTICOL_OFERTA =
@@ -2495,7 +2495,7 @@ function showRecipesList(data) {
             tbody4.appendChild(tr4)
             var td4 = document.createElement('td')
             td4.classList.add('border-0')
-            td4.innerHTML = object3.object.old_WBS
+            td4.innerHTML = `<del>${object3.object.old_WBS}</del>`;
             tr4.appendChild(td4)
           }
           //add denumire_articol_oferta, um_articol_oferta, tip_articol_oferta, subtip_articol_oferta too
