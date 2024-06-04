70c3d14393c4f7bbbb9d1dbddcbe34c055b482a9
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-06-04 18:42:09 +0300

    Refactor client.js to simplify duplicate reteta object elimination and improve table cell creation in Activity class

--- public/client.js
+++ public/client.js
@@ -2548,67 +2548,26 @@ function eleminateDuplicates(data) {
 
   for (let i = 0; i < innerData.length; i++) {
     let reteta = innerData[i].reteta
-    let activitati = []
-    reteta.forEach(function (obj) {
-      activitateCuCriterii = {
-        DENUMIRE_ARTICOL_OFERTA: obj.object.DENUMIRE_ARTICOL_OFERTA,
-        UM_ARTICOL_OFERTA: obj.object.UM_ARTICOL_OFERTA,
-        TIP_ARTICOL_OFERTA: obj.object.TIP_ARTICOL_OFERTA,
-        SUBTIP_ARTICOL_OFERTA: obj.object.SUBTIP_ARTICOL_OFERTA
+    for (let j = 0; j<reteta.length; j++) {
+      let obj = reteta[j]
+      let children = obj.children
+      let objProps = [obj.object.DENUMIRE_ARTICOL_OFERTA, obj.object.UM_ARTICOL_OFERTA, obj.object.TIP_ARTICOL_OFERTA, obj.object.SUBTIP_ARTICOL_OFERTA]
+      let childrenProps = children.map((child) => [child.object.DENUMIRE_ARTICOL_OFERTA, child.object.UM_ARTICOL_OFERTA, child.object.TIP_ARTICOL_OFERTA, child.object.SUBTIP_ARTICOL_OFERTA])
+      let found = innerData.filter((reteta) => {
+        let retetaProps = [reteta.object.DENUMIRE_ARTICOL_OFERTA, reteta.object.UM_ARTICOL_OFERTA, reteta.object.TIP_ARTICOL_OFERTA, reteta.object.SUBTIP_ARTICOL_OFERTA]
+        let children = reteta.children
+        let childrenProps = children.map((child) => [child.object.DENUMIRE_ARTICOL_OFERTA, child.object.UM_ARTICOL_OFERTA, child.object.TIP_ARTICOL_OFERTA, child.object.SUBTIP_ARTICOL_OFERTA])
+        return JSON.stringify(objProps) == JSON.stringify(retetaProps) && JSON.stringify(childrenProps) == JSON.stringify(childrenProps)
+      })
+      if (found.length == 1) {
+        result.push(innerData[i])
       }
-      let children = []
-      obj.children.forEach(function (child) {
-        children.push({
-          DENUMIRE_ARTICOL_OFERTA: child.object.DENUMIRE_ARTICOL_OFERTA,
-          UM_ARTICOL_OFERTA: child.object.UM_ARTICOL_OFERTA,
-          TIP_ARTICOL_OFERTA: child.object.TIP_ARTICOL_OFERTA,
-          SUBTIP_ARTICOL_OFERTA: child.object.SUBTIP_ARTICOL_OFERTA
-        })
-      })
-
-      activitati.push({ activitate: activitateCuCriterii, materialeActivitate: children })
-    })
-    //take 1 and compare with the rest, up and down
-    let rest = [...activitati]
-    let result = []
-    
-    for (let j = 0; j < rest.length; j++) {
-      let activitate = rest[j]
-      let materialeActivitate = activitate.materialeActivitate
-      let restActivitati = [...rest]
-      restActivitati.splice(j, 1)
-      let restActivitatiFiltrate = []
-      restActivitati.forEach(function (restActivitate) {
-        let restMaterialeActivitate = restActivitate.materialeActivitate
-        let filtrate = materialeActivitate.filter((materiale) => {
-          let restMateriale = restMaterialeActivitate.find(
-            (restMateriale) =>
-              materiale.DENUMIRE_ARTICOL_OFERTA == restMateriale.DENUMIRE_ARTICOL_OFERTA &&
-              materiale.UM_ARTICOL_OFERTA == restMateriale.UM_ARTICOL_OFERTA &&
-              materiale.TIP_ARTICOL_OFERTA == restMateriale.TIP_ARTICOL_OFERTA &&
-              materiale.SUBTIP_ARTICOL_OFERTA == restMateriale.SUBTIP_ARTICOL_OFERTA
-          )
-          if (restMateriale) {
-            return true
-          } else {
-            return false
-          }
-        })
-        if (filtrate.length == materialeActivitate.length) {
-          restActivitatiFiltrate.push(restActivitate)
-        }
-      })
-      //console.log('restActivitatiFiltrate', restActivitatiFiltrate)
-      restActivitatiFiltrate.forEach(function (restActivitateFiltrata) {
-        let index = rest.indexOf(restActivitateFiltrata)
-        rest.splice(index, 1)
-      })
     }
   }
 
   console.log('result', result)
 
-  return result
+  return result    
 }
 
 /*
