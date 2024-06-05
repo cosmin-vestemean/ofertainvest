ac9c70a2baa7e89762b546913a4db0efb2c35d0b
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-06-05 09:48:08 +0300

    Refactor client.js to eliminate duplicate reteta objects and their children

--- public/client.js
+++ public/client.js
@@ -2583,6 +2583,7 @@ function eleminateDuplicates(data) {
           instanteRetete[k].duplicate = true
           //point to the original reteta
           instanteRetete[k].duplicateOf = i
+          instanteRetete.cantitateAntemasuratori = 0
           //transform reteta in instanta sa proprie
           instanteRetete[i].duplicate = true
           instanteRetete[i].duplicateOf = i
