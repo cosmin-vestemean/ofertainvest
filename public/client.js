4e664394fa0475619e85a4e14d9a904edeae72f4
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-07-18 19:29:17 +0300

    Refactor client.js: console.log, looking for a bug

--- public/client.js
+++ public/client.js
@@ -2901,6 +2901,7 @@ function createTreesFromWBS(ds) {
 
   //returns old WBS and newly created WBS, if any
   instanteRetete = applyFilterChildrenEndsWith0(instanteRetete)
+  console.log('instanteRetete1', [...instanteRetete])
 
   instanteRetete = prepareForMultipleActivities(instanteRetete)
   console.log('instanteRetete', [...instanteRetete])
