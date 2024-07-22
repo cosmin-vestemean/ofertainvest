7c062200f4996a312013e6144e13c3b5cf1e2dd0
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-07-22 18:37:20 +0300

    Refactor client.js: Add missing levels to antemasuratori display mask and set default values for empty fields

--- public/client.js
+++ public/client.js
@@ -151,12 +151,6 @@ const antemasuratoriDisplayMask = {
     visible: true,
     label: 'Cantitate oferta'
   },
-  CANTITATE_ARTICOL_ANTEMASURATORI: {
-    value: 'CANITATE_ARTICOL_ANTEMASURATORI',
-    RW: true,
-    visible: true,
-    label: 'Cantitate antemasuratori'
-  },
   UM_ARTICOL_OFERTA: { value: 'UM_ARTICOL_OFERTA', RW: false, visible: true, label: 'UM' },
   TIP_ARTICOL_OFERTA: { value: TIP_ARTICOL_OFERTA, RW: false, visible: false, label: 'Tip articol' },
   SUBTIP_ARTICOL_OFERTA: { value: SUBTIP_ARTICOL_OFERTA, RW: false, visible: false, label: 'Subtip articol' },
@@ -169,7 +163,13 @@ const antemasuratoriDisplayMask = {
   NIVEL_OFERTA_7: { value: 'NIVEL_OFERTA_7', RW: false, visible: true, label: 'Nivel 7' },
   NIVEL_OFERTA_8: { value: 'NIVEL_OFERTA_8', RW: false, visible: true, label: 'Nivel 8' },
   NIVEL_OFERTA_9: { value: 'NIVEL_OFERTA_9', RW: false, visible: true, label: 'Nivel 9' },
-  NIVEL_OFERTA_10: { value: 'NIVEL_OFERTA_10', RW: false, visible: true, label: 'Nivel 10' }
+  NIVEL_OFERTA_10: { value: 'NIVEL_OFERTA_10', RW: false, visible: true, label: 'Nivel 10' },
+  CANTITATE_ARTICOL_ANTEMASURATORI: {
+    value: 'CANITATE_ARTICOL_ANTEMASURATORI',
+    RW: true,
+    visible: true,
+    label: 'Cantitate antemasuratori'
+  }
 }
 
 const themes = ['cerulean', 'flatly', 'sandstone', 'stylish', 'yeti']
