0dcc0f72cf2ec3486e7007f12abf6208d98754a0
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-07-25 17:13:31 +0300

    Refactor client.js: Fix bad reference in estimari class

--- public/client.js
+++ public/client.js
@@ -3063,7 +3063,7 @@ class estimari extends LitElement {
         let maxLevelA = this.ds[0][0].antemasuratori[0].branch[0].length
         //gaseste nivelul maxim din o; adica numara cate _nivel_oferta sunt in o
         //adauga la o diferenta de niveluri
-        let keys = Object.keys(o)
+        let keys = Object.keys(firstLine)
         let maxLevelObject = 0
         for (let key of keys) {
           if (key.includes(_nivel_oferta)) {
