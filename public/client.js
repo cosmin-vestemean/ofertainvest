8699457d09f3dc52be5c1afbb902193f3c2fb498
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-07-29 13:33:23 +0300

    Refactor client.js: Improve collapsible functionality for table rows

--- public/client.js
+++ public/client.js
@@ -3096,7 +3096,7 @@ class estimari extends LitElement {
         plus_icon.style.cursor = 'pointer'
         plus_icon.onclick = function () {
           //show hide all children, identified by same id and a "_some_number"
-          var children = document.getElementById('my_table_estimari').renderRoot.getElementById('tbody_estimari').querySelectorAll('[id^=' + i + '@' + k + '_]')
+          var children = document.getElementById('my_table_estimari').renderRoot.getElementById('tbody_estimari').querySelectorAll('[id^=' + i + '@' + k  + '_]')
           for (let i = 0; i < children.length; i++) {
             if (children[i].classList.contains('d-none')) {
               children[i].classList.remove('d-none')
