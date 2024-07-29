3043ca41b31486fc7e62ab5a439bc07ffb2b53eb
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-07-29 14:29:12 +0300

    Refactor client.js: Update variable name for estimari pool data structure

--- public/client.js
+++ public/client.js
@@ -3192,7 +3192,7 @@ class estimari extends LitElement {
                 } else {
                   ramura = position[1]
                 }
-                ds_estimari_pool[instanta][ramura][activitateIndex][key] = parseFloat(e.target.textContent.trim());
+                ds_estimari_pool[instanta][ramura][activitateIndex].row_data[key] = parseFloat(e.target.textContent.trim());
                 localStorage.setItem('ds_estimari_pool', JSON.stringify(ds))
               }
             })
