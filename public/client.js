7e4b50a6c7537978e1b24daa9b8fc75ea2f58cf5
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-08-01 12:25:11 +0300

    Refactor client.js: Remove unused table-mobile-responsive class and update selected theme to 'cerulean' by default

--- public/client.js
+++ public/client.js
@@ -254,14 +254,14 @@ const themes = ['cerulean', 'flatly', 'sandstone', 'stylish', 'yeti']
 let selectedTheme = 'cerulean'
 export let template = document.createElement('template')
 template.innerHTML = `
+<link rel="stylesheet" href="client.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"/><link rel="stylesheet" href="${selectedTheme}.css">
 <script
   src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
   crossorigin="anonymous">
-</script>
-<link rel="stylesheet" href="client.css">`
+</script>`
 
 // 1. load excel file by file chooser xlsx.js
 function loadDataFromFile(evt) {
