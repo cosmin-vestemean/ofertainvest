9eebbb2fd7341ab7d268fa6dd54ecf212fab11c3
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-08-01 17:22:55 +0300

    Refactor estimari.js: Filter and save selected estimari with non-zero quantity

--- public/estimari.js
+++ public/estimari.js
@@ -119,7 +119,7 @@ export class estimari extends LitElement {
       save_icon.style.marginLeft = '5px';
       save_icon.onclick = () => {
         //filter ds_estimari_flat with key ROW_SELECTED = true and parseFloat(_cantitate_estimari) > 0
-        let ds_estimari_flat_filterd = ds_estimari_flat.filter(
+        const ds_estimari_flat_filterd = ds_estimari_flat.filter(
           (o) => o.ROW_SELECTED && parseFloat(o[_cantitate_estimari]) > 0
         );
         //change color to green
