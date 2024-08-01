c552e84f592a9953398ef8189d8832445b477c1a
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-08-01 14:03:22 +0300

    Refactor client.js: Remove unnecessary code and improve theme change logic

--- public/client.js
+++ public/client.js
@@ -1787,8 +1787,6 @@ export function init() {
       selectedTheme = theme
       changeTheme(theme)
       console.log('Theme changed to:', selectedTheme)
-      //get template
-      template = document.getElementById('shadowRootTemplate')
       themeLink = selectedTheme === 'default' ? '' : `<link rel="stylesheet" href="${selectedTheme}.css">`
       template.innerHTML = `
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
