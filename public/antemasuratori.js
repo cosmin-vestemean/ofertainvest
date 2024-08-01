79ad8afe4998592f77e562942a8d5a1d66ab7560
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-08-01 11:11:38 +0300

    Refactor client.js: new module: antemasuratori class in new file

---
+++ public/antemasuratori.js
@@ -0,0 +1,196 @@
+import { LitElement, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+import { template, _cantitate_antemasuratori, theadIsSet, antemasuratoriDisplayMask, _cantitate_oferta, ds_antemasuratori, newTree } from "./client.js";
+
+export class antemasuratori extends LitElement {
+  static properties = {
+    ds: { type: Array }
+  };
+
+  constructor() {
+    super();
+    this.ds = [];
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    //add event listener for keydown for td class cantitate_antemasuratori
+    this.shadowRoot.addEventListener('keydown', function (e) {
+      if (e.target.classList.contains(_cantitate_antemasuratori)) {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          e.target.blur();
+        }
+      }
+    });
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    console.log('antemasuratori element added to the DOM');
+  }
+
+  render() {
+    console.log('rendering antemasuratori element with following array', this.ds, 'added at', new Date());
+
+    if (!this.ds || this.ds.length == 0) {
+      return html`<p class="label label-danger">No data</p>`;
+    } else {
+      //add table
+      var table = document.createElement('table');
+      table.classList.add('table');
+      table.classList.add('table-sm');
+      table.classList.add('table-hover');
+      table.classList.add('table-responsive');
+      table.id = 'table_antemasuratori';
+      //font size
+      table.style.fontSize = 'small';
+      //get or create thead and tbody
+      var thead = document.createElement('thead');
+      thead.id = 'thead_antemasuratori';
+      thead.classList.add('align-middle');
+      var tbody = document.createElement('tbody');
+      tbody.id = 'tbody_antemasuratori';
+      if (theadIsSet) {
+        tbody.classList.add('table-group-divider');
+      }
+      table.appendChild(tbody);
+      //add thead
+      if (theadIsSet) {
+        table.appendChild(thead);
+        var tr = document.createElement('tr');
+        thead.appendChild(tr);
+        //append counter
+        var th = document.createElement('th');
+        th.scope = 'col';
+        tr.appendChild(th);
+        for (var key in antemasuratoriDisplayMask) {
+          //check key vs antemasuratoriDisplayMask
+          //first check if key exists in antemasuratoriDisplayMask
+          if (Object.keys(this.ds[0]).includes(key)) {
+            //check if visible
+            if (antemasuratoriDisplayMask[key].visible) {
+              var th = document.createElement('th');
+              th.scope = 'col';
+              th.style.writingMode = 'vertical-rl';
+              th.style.rotate = '180deg';
+              if (!antemasuratoriDisplayMask[key].isEnumerable) {
+                th.innerHTML = antemasuratoriDisplayMask[key].label
+                  ? antemasuratoriDisplayMask[key].label
+                  : key;
+              } else {
+                th.innerHTML = antemasuratoriDisplayMask[key].label
+                  ? antemasuratoriDisplayMask[key].label
+                  : key;
+                //insert select element with multiple selections
+              }
+
+              tr.appendChild(th);
+            }
+          }
+        }
+      }
+      //add tbody
+      let counter = 0;
+      this.ds.forEach(function (object) {
+        counter++;
+        var tr = document.createElement('tr');
+        tbody.appendChild(tr);
+        var td = document.createElement('td');
+        td.style.fontWeight = 'bold';
+        td.innerHTML = counter;
+        tr.appendChild(td);
+        for (var key in antemasuratoriDisplayMask) {
+          //check key vs antemasuratoriDisplayMask
+          //first check if key exists in antemasuratoriDisplayMask
+          if (Object.keys(object).includes(key)) {
+            //check if visible
+            if (antemasuratoriDisplayMask[key].visible) {
+              var td = document.createElement('td');
+              td.innerHTML =
+                typeof object[key] === 'number' ? object[key].toFixed(2) : object[key] ? object[key] : '';
+              if (key == _cantitate_oferta || key == _cantitate_antemasuratori) {
+                td.style.fontWeight = 'bold';
+              }
+              //cantitate antemasuratori contenteditable
+              if (key == _cantitate_antemasuratori) {
+                let customClass = _cantitate_antemasuratori;
+                td.spellcheck = false;
+                td.classList.add('border', 'text-primary');
+                td.classList.add(customClass);
+                td.style.borderColor = 'lightgray';
+                //add blur (focusout) event
+                td.addEventListener('focusout', function (e) {
+                  var index = Array.from(
+                    document
+                      .getElementById('my_table_antemasuratori')
+                      .shadowRoot.getElementById('tbody_antemasuratori')
+                      .querySelectorAll('.' + _cantitate_antemasuratori)
+                  ).indexOf(e.target);
+                  console.log('index tr antemas', index);
+                  ds_antemasuratori[index][_cantitate_antemasuratori] = parseFloat(e.target.textContent);
+                  //replace in local storage
+                  localStorage.setItem('ds_antemasuratori', JSON.stringify(ds_antemasuratori));
+                  //update newTree
+                  let branch = newTree[ds_antemasuratori[index].refInstanta][ds_antemasuratori[index].refActivitate].antemasuratori.find((o) => o.branch.join() === ds_antemasuratori[index].refBranch.join());
+                  if (branch) branch.qty = parseFloat(e.target.textContent);
+                  //update newTree in local storage
+                  localStorage.setItem('newTree', JSON.stringify(newTree));
+                });
+
+                //add keydown event arrow up/down to move to prior/next td _cantitate_antemasuratori
+                td.addEventListener('keydown', function (e) {
+                  if (e.key === 'ArrowUp') {
+                    e.preventDefault();
+                    var index = Array.from(
+                      document
+                        .getElementById('my_table_antemasuratori')
+                        .shadowRoot.getElementById('tbody_antemasuratori')
+                        .querySelectorAll('.' + _cantitate_antemasuratori)
+                    ).indexOf(e.target);
+                    if (index > 0) {
+                      var tds = document
+                        .getElementById('my_table_antemasuratori')
+                        .shadowRoot.getElementById('tbody_antemasuratori')
+                        .querySelectorAll('.' + _cantitate_antemasuratori);
+                      tds[index - 1].focus();
+                    }
+                  }
+                  if (e.key === 'ArrowDown') {
+                    e.preventDefault();
+                    var index = Array.from(
+                      document
+                        .getElementById('my_table_antemasuratori')
+                        .shadowRoot.getElementById('tbody_antemasuratori')
+                        .querySelectorAll('.' + _cantitate_antemasuratori)
+                    ).indexOf(e.target);
+                    if (index < ds_antemasuratori.length - 1) {
+                      var tds = document
+                        .getElementById('my_table_antemasuratori')
+                        .shadowRoot.getElementById('tbody_antemasuratori')
+                        .querySelectorAll('.' + _cantitate_antemasuratori);
+                      tds[index + 1].focus();
+                    }
+                  }
+                });
+
+                //select all inner html in cell on focusin
+                td.addEventListener('focusin', function (e) {
+                  var range = document.createRange();
+                  range.selectNodeContents(e.target);
+                  var sel = window.getSelection();
+                  sel.removeAllRanges();
+                  sel.addRange(range);
+                });
+              }
+              //check if RW => td.contentEditable = true
+              if (antemasuratoriDisplayMask[key].RW) {
+                td.contentEditable = true;
+              }
+              tr.appendChild(td);
+            }
+          }
+        }
+      });
+
+      return html`${table}`;
+    }
+  }
+}
--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
 import { LitElement, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js'
 import { estimari } from './estimari.js'
 import { myTable } from './myTable.js'
+import { antemasuratori } from './antemasuratori.js'
 
 const TIP_ARTICOL_OFERTA = ['ARTICOL', 'SUBARTICOL', 'MATERIAL']
 const SUBTIP_ARTICOL_OFERTA = [
@@ -58,7 +59,7 @@ var recipes_ds = []
 var combinatii_unice = []
 var selected_ds = []
 var ds_instanteRetete = []
-var ds_antemasuratori = []
+export var ds_antemasuratori = []
 export var ds_estimari_pool = []
 export var ds_estimari_flat = []
 export var ds_estimari = []
@@ -86,7 +87,7 @@ export let newTree = []
 var trees = []
 var niveluri = []
 var _nivel_oferta = 'NIVEL_OFERTA_'
-var _cantitate_oferta = 'CANTITATE_ARTICOL_OFERTA'
+export var _cantitate_oferta = 'CANTITATE_ARTICOL_OFERTA'
 export var _cantitate_antemasuratori = 'CANTITATE_ARTICOL_ANTEMASURATORI'
 export var _cantitate_estimari = 'CANTITATE_ARTICOL_ESTIMARI'
 var _grupare_oferta = 'GRUPARE_ARTICOL_OFERTA'
@@ -139,7 +140,7 @@ const recipeDisplayMask = {
   }
 }
 
-const antemasuratoriDisplayMask = {
+export const antemasuratoriDisplayMask = {
   old_WBS: { value: 'old_WBS', RW: false, visible: false, label: 'WBS vechi' },
   WBS: { value: 'WBS', RW: false, visible: false, label: 'WBS' },
   SERIE_ARTICOL_OFERTA: {
@@ -2611,204 +2612,6 @@ class Activity extends LitElement {
 
 customElements.define('my-activity', Activity)
 
-class antemasuratori extends LitElement {
-  static properties = {
-    ds: { type: Array }
-  }
-
-  constructor() {
-    super()
-    this.ds = []
-    this.attachShadow({ mode: 'open' })
-    this.shadowRoot.appendChild(template.content.cloneNode(true))
-    //add event listener for keydown for td class cantitate_antemasuratori
-    this.shadowRoot.addEventListener('keydown', function (e) {
-      if (e.target.classList.contains(_cantitate_antemasuratori)) {
-        if (e.key === 'Enter') {
-          e.preventDefault()
-          e.target.blur()
-        }
-      }
-    })
-  }
-
-  connectedCallback() {
-    super.connectedCallback()
-    console.log('antemasuratori element added to the DOM')
-  }
-
-  render() {
-    console.log('rendering antemasuratori element with following array', this.ds, 'added at', new Date())
-
-    if (!this.ds || this.ds.length == 0) {
-      return html`<p class="label label-danger">No data</p>`
-    } else {
-      //add table
-      var table = document.createElement('table')
-      table.classList.add('table')
-      table.classList.add('table-sm')
-      table.classList.add('table-hover')
-      table.classList.add('table-responsive')
-      table.id = 'table_antemasuratori'
-      //font size
-      table.style.fontSize = 'small'
-      //get or create thead and tbody
-      var thead = document.createElement('thead')
-      thead.id = 'thead_antemasuratori'
-      thead.classList.add('align-middle')
-      var tbody = document.createElement('tbody')
-      tbody.id = 'tbody_antemasuratori'
-      if (theadIsSet) {
-        tbody.classList.add('table-group-divider')
-      }
-      table.appendChild(tbody)
-      //add thead
-      if (theadIsSet) {
-        table.appendChild(thead)
-        var tr = document.createElement('tr')
-        thead.appendChild(tr)
-        //append counter
-        var th = document.createElement('th')
-        th.scope = 'col'
-        tr.appendChild(th)
-        for (var key in antemasuratoriDisplayMask) {
-          //check key vs antemasuratoriDisplayMask
-          //first check if key exists in antemasuratoriDisplayMask
-          if (Object.keys(this.ds[0]).includes(key)) {
-            //check if visible
-            if (antemasuratoriDisplayMask[key].visible) {
-              var th = document.createElement('th')
-              th.scope = 'col'
-              th.style.writingMode = 'vertical-rl'
-              th.style.rotate = '180deg'
-              if (!antemasuratoriDisplayMask[key].isEnumerable) {
-                th.innerHTML = antemasuratoriDisplayMask[key].label
-                  ? antemasuratoriDisplayMask[key].label
-                  : key
-              } else {
-                th.innerHTML = antemasuratoriDisplayMask[key].label
-                  ? antemasuratoriDisplayMask[key].label
-                  : key
-                //insert select element with multiple selections
-              }
-
-              tr.appendChild(th)
-            }
-          }
-        }
-      }
-      //add tbody
-      let counter = 0
-      this.ds.forEach(function (object) {
-        counter++
-        var tr = document.createElement('tr')
-        tbody.appendChild(tr)
-        var td = document.createElement('td')
-        td.style.fontWeight = 'bold'
-        td.innerHTML = counter
-        tr.appendChild(td)
-        for (var key in antemasuratoriDisplayMask) {
-          //check key vs antemasuratoriDisplayMask
-          //first check if key exists in antemasuratoriDisplayMask
-          if (Object.keys(object).includes(key)) {
-            //check if visible
-            if (antemasuratoriDisplayMask[key].visible) {
-              var td = document.createElement('td')
-              td.innerHTML =
-                typeof object[key] === 'number' ? object[key].toFixed(2) : object[key] ? object[key] : ''
-              if (key == _cantitate_oferta || key == _cantitate_antemasuratori) {
-                td.style.fontWeight = 'bold'
-              }
-              //cantitate antemasuratori contenteditable
-              if (key == _cantitate_antemasuratori) {
-                let customClass = _cantitate_antemasuratori
-                td.spellcheck = false
-                td.classList.add('border', 'text-primary')
-                td.classList.add(customClass)
-                td.style.borderColor = 'lightgray'
-                //add blur (focusout) event
-                td.addEventListener('focusout', function (e) {
-                  var index = Array.from(
-                    document
-                      .getElementById('my_table_antemasuratori')
-                      .shadowRoot.getElementById('tbody_antemasuratori')
-                      .querySelectorAll('.' + _cantitate_antemasuratori)
-                  ).indexOf(e.target)
-                  console.log('index tr antemas', index)
-                  ds_antemasuratori[index][_cantitate_antemasuratori] = parseFloat(e.target.textContent)
-                  //replace in local storage
-                  localStorage.setItem('ds_antemasuratori', JSON.stringify(ds_antemasuratori))
-                  //update newTree
-                  let branch = newTree[ds_antemasuratori[index].refInstanta][
-                    ds_antemasuratori[index].refActivitate
-                  ].antemasuratori.find((o) => o.branch.join() === ds_antemasuratori[index].refBranch.join())
-                  if (branch) branch.qty = parseFloat(e.target.textContent)
-                  //update newTree in local storage
-                  localStorage.setItem('newTree', JSON.stringify(newTree))
-                })
-
-                //add keydown event arrow up/down to move to prior/next td _cantitate_antemasuratori
-                td.addEventListener('keydown', function (e) {
-                  if (e.key === 'ArrowUp') {
-                    e.preventDefault()
-                    var index = Array.from(
-                      document
-                        .getElementById('my_table_antemasuratori')
-                        .shadowRoot.getElementById('tbody_antemasuratori')
-                        .querySelectorAll('.' + _cantitate_antemasuratori)
-                    ).indexOf(e.target)
-                    if (index > 0) {
-                      var tds = document
-                        .getElementById('my_table_antemasuratori')
-                        .shadowRoot.getElementById('tbody_antemasuratori')
-                        .querySelectorAll('.' + _cantitate_antemasuratori)
-                      tds[index - 1].focus()
-                    }
-                  }
-                  if (e.key === 'ArrowDown') {
-                    e.preventDefault()
-                    var index = Array.from(
-                      document
-                        .getElementById('my_table_antemasuratori')
-                        .shadowRoot.getElementById('tbody_antemasuratori')
-                        .querySelectorAll('.' + _cantitate_antemasuratori)
-                    ).indexOf(e.target)
-                    if (index < ds_antemasuratori.length - 1) {
-                      var tds = document
-                        .getElementById('my_table_antemasuratori')
-                        .shadowRoot.getElementById('tbody_antemasuratori')
-                        .querySelectorAll('.' + _cantitate_antemasuratori)
-                      tds[index + 1].focus()
-                    }
-                  }
-                })
-
-                //select all inner html in cell on focusin
-                td.addEventListener('focusin', function (e) {
-                  var range = document.createRange()
-                  range.selectNodeContents(e.target)
-                  var sel = window.getSelection()
-                  sel.removeAllRanges()
-                  sel.addRange(range)
-                })
-              }
-              //check if RW => td.contentEditable = true
-              if (antemasuratoriDisplayMask[key].RW) {
-                td.contentEditable = true
-              }
-              tr.appendChild(td)
-            }
-          }
-        }
-      })
-
-      return html`${table}`
-    }
-  }
-}
-
-customElements.define('my-antemasuratori', antemasuratori)
-
 export function locateTrInEstimariPool(htmlElement) {
   //check for undefined, null, empty or NaN
   if (!htmlElement) {
@@ -3857,4 +3660,5 @@ function showHideHeader() {
 }
 
 customElements.define('my-table', myTable)
+customElements.define('my-antemasuratori', antemasuratori)
 customElements.define('my-estimari', estimari)
