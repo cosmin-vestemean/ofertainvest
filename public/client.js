36564ef6dd624a313ad629521e9c2b0a1c197619
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-08-06 15:01:01 +0300

    Refactor database schema: Add tables CCCOFERTEWEB, CCCAFLWEB, and CCCLINIIWEB Construct S1 structure

--- public/client.js
+++ public/client.js
@@ -269,7 +269,7 @@ function loadDataFromFile(evt) {
 
   var btn_oferta = document.getElementById('btn_oferta')
   var dl = `<i class="bi bi-download"></i>`;
-  btn_oferta.innerHTML = 'Salveaza oferta initiala ' + dl;
+  btn_oferta.innerHTML = 'Salveaza oferta ' + dl;
   btn_oferta.classList.remove('btn-success')
   btn_oferta.classList.add('btn-danger')
 }
@@ -838,10 +838,7 @@ async function saveOferta() {
     return
   }
 
-  var hourglassIcon =
-    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hourglass-split" viewBox="0 0 16 16">' +
-    '<path d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z"/>' +
-    '</svg>'
+  var hourglassIcon = '<i class="bi bi-hourglass-split"></i>'
   var btn_oferta = document.getElementById('btn_oferta')
   btn_oferta.innerHTML = hourglassIcon + ' Salvare...'
   //bg-warning
@@ -1263,112 +1260,8 @@ export function init() {
     showRecipesList(recipes_ds)
   }
   document.getElementById('trndate').valueAsDate = new Date()
-  let select_trdr = document.getElementById('trdr')
-  //populate select_trdr by calling S1 service getDataset
-  connectToS1Service()
-    .then(async (result) => {
-      const clientID = result.token
-      console.log('clientID', clientID)
-      var params = {
-        query: {
-          clientID: clientID,
-          appID: '1001',
-          sqlQuery: 'select TRDR, NAME from trdr where sodtype=13 and isactive=1 order by NAME asc'
-        }
-      }
-
-      await client
-        .service('getDataset')
-        .find(params)
-        .then((result) => {
-          console.log('result', result)
-          if (result.success) {
-            //populate select_trdr
-            result.data.forEach(function (object) {
-              var option = document.createElement('option')
-              option.value = object['TRDR']
-              option.text = object['NAME']
-              select_trdr.appendChild(option)
-            })
-            select_trdr.selectedIndex = -1
-          } else {
-            console.log('error', result.error)
-          }
-          //select id="prjc" populate by calling S1 service getDataset
-          var select_prjc = document.getElementById('prjc')
-          var params = {
-            query: {
-              clientID: clientID,
-              appID: '1001',
-              sqlQuery:
-                'select PRJC, NAME from prjc where isactive=1 and ' +
-                //not olde than 2 years
-                'insdate > dateadd(year, -2, getdate()) ' +
-                'order by insdate desc'
-            }
-          }
-
-          client
-            .service('getDataset')
-            .find(params)
-            .then((result) => {
-              console.log('result', result)
-              if (result.success) {
-                //populate select_prjc
-                result.data.forEach(function (object) {
-                  var option = document.createElement('option')
-                  option.value = object['PRJC']
-                  option.text = object['NAME']
-                  select_prjc.appendChild(option)
-                })
-                select_prjc.selectedIndex = -1
-                //populate saldoc by calling S1 service getDataset
-                var select_saldoc = document.getElementById('saldoc')
-                var params = {
-                  query: {
-                    clientID: clientID,
-                    appID: '1001',
-                    sqlQuery:
-                      'select FINDOC, FINCODE from findoc where iscancel=0 and sosource=1351 and fprms=4001 and series=4002 order by trndate desc, findoc desc'
-                  }
-                }
-
-                client
-                  .service('getDataset')
-                  .find(params)
-                  .then((result) => {
-                    console.log('result', result)
-                    if (result.success) {
-                      //populate select_saldoc
-                      result.data.forEach(function (object) {
-                        var option = document.createElement('option')
-                        option.value = object['FINDOC']
-                        option.text = object['FINCODE']
-                        select_saldoc.appendChild(option)
-                      })
-                      select_saldoc.selectedIndex = -1
-                    } else {
-                      console.log('error', result.error)
-                    }
-                  })
-                  .catch((error) => {
-                    console.log('error', error)
-                  })
-              } else {
-                console.log('error', result.error)
-              }
-            })
-            .catch((error) => {
-              console.log('error', error)
-            })
-        })
-        .catch((error) => {
-          console.log('error', error)
-        })
-    })
-    .catch((error) => {
-      console.log('error', error)
-    })
+  //populate selects trdr, prjc, oferta by calling S1 service getDataset
+  S1.populateSelects()
 
   //add on hover to my_table2 rows and get index of row and look in recipes_ds[index] for reteta to display in my_table2
   //do not forget shadowRoot
@@ -3700,6 +3593,116 @@ class listaEstimari extends LitElement {
     }
 
     return html`${table}`
+  }
+}
+
+const S1 = {
+  populateSelects: function () {
+    connectToS1Service()
+    .then(async (result) => {
+      const clientID = result.token
+      console.log('clientID', clientID)
+      var params = {
+        query: {
+          clientID: clientID,
+          appID: '1001',
+          sqlQuery: 'select TRDR, NAME from trdr where sodtype=13 and isactive=1 order by NAME asc'
+        }
+      }
+
+      await client
+        .service('getDataset')
+        .find(params)
+        .then((result) => {
+          console.log('result', result)
+          if (result.success) {
+            var select_trdr = document.getElementById('trdr')
+            //populate select_trdr
+            result.data.forEach(function (object) {
+              var option = document.createElement('option')
+              option.value = object['TRDR']
+              option.text = object['NAME']
+              select_trdr.appendChild(option)
+            })
+            select_trdr.selectedIndex = -1
+          } else {
+            console.log('error', result.error)
+          }
+          //select id="prjc" populate by calling S1 service getDataset
+          var params = {
+            query: {
+              clientID: clientID,
+              appID: '1001',
+              sqlQuery:
+                'select PRJC, NAME from prjc where isactive=1 and ' +
+                //not olde than 2 years
+                'insdate > dateadd(year, -2, getdate()) ' +
+                'order by insdate desc'
+            }
+          }
+
+          client
+            .service('getDataset')
+            .find(params)
+            .then((result) => {
+              console.log('result', result)
+              if (result.success) {
+                var select_prjc = document.getElementById('prjc')
+                //populate select_prjc
+                result.data.forEach(function (object) {
+                  var option = document.createElement('option')
+                  option.value = object['PRJC']
+                  option.text = object['NAME']
+                  select_prjc.appendChild(option)
+                })
+                select_prjc.selectedIndex = -1
+                //populate saldoc by calling S1 service getDataset
+                var params = {
+                  query: {
+                    clientID: clientID,
+                    appID: '1001',
+                    sqlQuery:
+                      'select FINDOC, FINCODE from findoc where iscancel=0 and sosource=1351 and fprms=4001 and series=4002 order by trndate desc, findoc desc'
+                  }
+                }
+
+                client
+                  .service('getDataset')
+                  .find(params)
+                  .then((result) => {
+                    console.log('result', result)
+                    if (result.success) {
+                      var select_saldoc = document.getElementById('saldoc')
+                      //populate select_saldoc
+                      result.data.forEach(function (object) {
+                        var option = document.createElement('option')
+                        option.value = object['FINDOC']
+                        option.text = object['FINCODE']
+                        select_saldoc.appendChild(option)
+                      })
+                      select_saldoc.selectedIndex = -1
+                    } else {
+                      console.log('error', result.error)
+                    }
+                  })
+                  .catch((error) => {
+                    console.log('error', error)
+                  })
+              } else {
+                console.log('error', result.error)
+              }
+            })
+            .catch((error) => {
+              console.log('error', error)
+            })
+        })
+        .catch((error) => {
+          console.log('error', error)
+        })
+    })
+    .catch((error) => {
+      console.log('error', error)
+    })
   }
 }
 
