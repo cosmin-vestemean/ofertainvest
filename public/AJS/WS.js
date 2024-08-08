a873403c094a970a392dea7dd9f711cd58726221
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-08-08 17:29:14 +0300

    Refactor WS.js: Improve SQL transaction handling and error reporting

--- public/AJS/WS.js
+++ public/AJS/WS.js
@@ -44,38 +44,38 @@ function convertDatasetToArray(dataset) {
 }
 
 function runSQLTransaction(obj) {
-var result = { success: false, error: '' }
-if (!obj.sqlList || obj.sqlList.length == 0) {
+  var result = { success: false, error: '' }
+  if (!obj.sqlList || obj.sqlList.length == 0) {
     result.success = false
     result.error = 'No sql query transmitted.'
     return result
-} else {
+  } else {
     try {
-        var strSql = 'BEGIN TRY;' //start sql transaction with commit and rollback
-        //start sql transaction with commit and rollback
-        strSql += 'BEGIN TRANSACTION ;'
-        for (var i = 0; i < obj.sqlList.length; i++) {
-            strSql += obj.sqlList[i] + ';';
-        }
-        strSql += 'COMMIT;'
-        strSql += 'END TRY;'
-        strSql += 'BEGIN CATCH'
-        strSql += 'IF @@TRANCOUNT > 0'
-        strSql += 'BEGIN'
-        strSql += 'INSERT INTO CCCWEBERRORS'
-        strSql +=
-            'SELECT ERROR_NUMBER() AS ErrorNumber, ERROR_SEVERITY() AS ErrorSeverity, ERROR_STATE() AS ErrorState, ERROR_PROCEDURE() AS ErrorProcedure, ERROR_LINE() AS ErrorLine, ERROR_MESSAGE() AS ErrorMessage;'
-        strSql += 'ROLLBACK;'
-        strSql += 'END'
-        strSql += 'END CATCH;'
-        X.RUNSQL(strSql)
-        result.success = true
-        result.sql = strSql
-        return result
+      var strSql = 'BEGIN TRY;' //start sql transaction with commit and rollback
+      //start sql transaction with commit and rollback
+      strSql += 'BEGIN TRANSACTION ;'
+      for (var i = 0; i < obj.sqlList.length; i++) {
+        strSql += obj.sqlList[i] + ';' //add each sql query to the transaction
+      }
+      strSql += 'COMMIT '
+      strSql += 'END TRY '
+      strSql += 'BEGIN CATCH '
+      strSql += 'IF @@TRANCOUNT > 0 '
+      strSql += 'ROLLBACK '
+      strSql += 'END CATCH '
+      X.RUNSQL(strSql)
+      result.success = true
+      result.sql = strSql
+      return result
     } catch (e) {
-        result.error = e.message
-        result.success = false
-        return result
+      result.error = e.message
+      result.success = false
+      result.sql = strSql
+      strSql = 'INSERT INTO CCCWEBERRORS '
+      strSql +=
+        'SELECT ERROR_NUMBER() AS ErrorNumber, ERROR_SEVERITY() AS ErrorSeverity, ERROR_STATE() AS ErrorState, ERROR_PROCEDURE() AS ErrorProcedure, ERROR_LINE() AS ErrorLine, ERROR_MESSAGE() AS ErrorMessage '
+      X.RUNSQL(strSql)
+      return result
     }
-}
+  }
 }
