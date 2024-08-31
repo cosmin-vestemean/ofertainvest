0cddf199aa7f303374f6a050743a9620b79114ac
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-08-31 12:29:49 +0300

    Refactor S1.js to update uniqueNodesDB and responsePaths variables

--- public/utils/S1.js
+++ public/utils/S1.js
@@ -353,7 +353,7 @@ export async function saveTreesInDB(trees) {
   let responsePaths = []
 
   // Insert or update unique nodes
-  trees.forEach(async (tree) => {
+  for (const tree of trees) {
     for (let i = 0; i < tree.length; i++) {
       const branch = tree[i]
       for (let j = 0; j < branch.length; j++) {
@@ -363,7 +363,7 @@ export async function saveTreesInDB(trees) {
         }
       }
     }
-  })
+  }
 
   //get all unique nodes from DB
   const response = await client.service('getDataset').find({
