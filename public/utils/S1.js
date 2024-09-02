3408458e65b86b71e08bd9854fb6bfc975de1738
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-09-02 14:40:01 +0300

    Refactor S1.js to optimize WBS retrieval for saving recipes in the database

--- public/utils/S1.js
+++ public/utils/S1.js
@@ -300,7 +300,11 @@ export async function salveazaReteteInDB() {
         let m = r[i].children
         for (let k = 0; k < m.length; k++) {
           let material = m[k].object
-          let CCCOFERTEWEBLINII = await getValFromS1Query(`select CCCOFERTEWEBLINII from CCCOFERTEWEBLINII where CCCOFERTEWEB=${contextOferta.CCCOFERTEWEB} and DENUMIRE_ART_OF='${material.DENUMIRE_ARTICOL_OFERTA}' AND TIP_ART_OF='${material.TIP_ARTICOL_OFERTA}' AND SUBTIP_ART_OF='${material.SUBTIP_ARTICOL_OFERTA}' AND WBS = '${material.WBS}'`)
+          let mWBSTemp = material.WBS.split('.')
+          let mWBS0 = mWBSTemp.slice(0, mWBSTemp.length - 1).join('.') + '.0'
+          let mWBSL = mWBSTemp.slice(0, mWBSTemp.length - 1).join('.') + '.L'
+          let mWBS = `(${mWBS0} OR ${mWBSL}) OR ${material.WBS})`
+          let CCCOFERTEWEBLINII = await getValFromS1Query(`select CCCOFERTEWEBLINII from CCCOFERTEWEBLINII where CCCOFERTEWEB=${contextOferta.CCCOFERTEWEB} and DENUMIRE_ART_OF='${material.DENUMIRE_ARTICOL_OFERTA}' AND TIP_ART_OF='${material.TIP_ARTICOL_OFERTA}' AND SUBTIP_ART_OF='${material.SUBTIP_ARTICOL_OFERTA}' AND WBS = ${mWBS}`)
           let sql =
             `insert into CCCMATRETETE (CCCOFERTEWEB, CCCRETETE, CCCACTIVITRETETE, CCCOFERTEWEBLINII, PONDEREDECONT, PONDERENORMA, ISCUSTOM) ` +
             `values (${contextOferta.CCCOFERTEWEB}, ${parseInt(max) + j}, ${parseInt(maxActivitati) + i}, ${CCCOFERTEWEBLINII.value}, ${material.PONDERE_DECONT_ACTIVITATE_ARTICOL_RETETA || 0}, ${material.PONDERE_NORMA_ACTIVITATE_ARTICOL_RETETA || 0}, 0)`
