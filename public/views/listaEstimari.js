aa17aa1949d3c6fa136e4b31cb21909cdfeac04c
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-09-10 17:47:55 +0300

    Refactor listaEstimari.js: Update variable names for CCCPATHS and CANTITATE

--- public/views/listaEstimari.js
+++ public/views/listaEstimari.js
@@ -97,7 +97,7 @@ where a.cccoferteweb=1
 order by a.cccinstante, d.duplicateof, a.cccactivitinstante, f.path
 
   */
-  const query = `select a.cccinstante,
+  const query = `select a.CCCINSTANTE,
 	d.duplicateof,
 	c.id,
 	a.cccactivitinstante,
@@ -126,131 +126,6 @@ order by a.cccinstante, d.duplicateof, a.cccactivitinstante, f.path`
   console.log('response', response)
 
   let ds = response.data
-
-  /*
-  exemplu de ds
-  cccinstante	duplicateof	id	cccactivitinstante	cccactivitretete	cccantemasuratori	cccpaths	cantitate	ismain	iscustom
-1	0	0	1	1	2	7	60.0	1	0
-1	0	0	1	1	3	8	20.0	1	0
-1	0	0	1	1	1	6	20.0	1	0
-1	0	0	2	2	5	7		0	0
-1	0	0	2	2	6	8		0	0
-1	0	0	2	2	4	6		0	0
-1	0	0	3	3	8	7		0	0
-1	0	0	3	3	9	8		0	0
-1	0	0	3	3	7	6		0	0
-2	1	1	4	4	11	7		1	0
-2	1	1	4	4	12	8		1	0
-2	1	1	4	4	10	6		1	0
-3	2	2	5	5	14	7		1	0
-3	2	2	5	5	15	8		1	0
-3	2	2	5	5	13	6		1	0
-4	0		6		17	7			
-4	0		6		18	8			
-4	0		6		16	6			
-4	0		7		20	7			
-4	0		7		21	8			
-4	0		7		19	6			
-4	0		8		23	7			
-4	0		8		24	8			
-4	0		8		22	6			
-5	0		9		26	7			
-5	0		9		27	8			
-5	0		9		25	6			
-5	0		10		29	7			
-5	0		10		30	8			
-5	0		10		28	6			
-5	0		11		32	7			
-5	0		11		33	8			
-5	0		11		31	6			
-6	0		12		35	7			
-6	0		12		36	8			
-6	0		12		34	6			
-6	0		13		38	7			
-6	0		13		39	8			
-6	0		13		37	6			
-6	0		14		41	7			
-6	0		14		42	8			
-6	0		14		40	6			
-7	6	6	15	6	43	1		1	0
-8	7	7	16	7	44	3		1	0
-8	7	7	17	8	45	3		0	0
-8	7	7	18	9	46	3		0	0
-8	7	7	19	10	47	3		0	0
-8	7	7	20	11	48	3		0	0
-8	7	7	21	12	49	3		0	0
-8	7	7	22	13	50	3		0	0
-8	7	7	23	14	51	3		0	0
-8	7	7	24	15	52	3		0	0
-9	8	8	25	16	53	3		1	0
-10	8		26		54	3			
-*/
-
-  //pseduo code
-  //1. pentru fiecare duplicateof
-  //2. pentru fiecare cccinstante
-  //3. gsseste-o pe cea care are cccactivitretete not null
-  //4. gaseste-le pe cele care au cccactivitretete null si aplica-le cccactivitretete din cea gasita la pasul 3 (sunt instantele duplicate ale aceleasi retete)
-  //5. la fel si ismain si iscustom
-
-  //astfel rezultand
-  /*
-cccinstante	duplicateof	id	cccactivitinstante	cccactivitretete	cccantemasuratori	cccpaths	cantitate	ismain	iscustom
-1	0	0	1	1	2	7	60.0	1	0
-1	0	0	1	1	3	8	20.0	1	0
-1	0	0	1	1	1	6	20.0	1	0
-1	0	0	2	2	5	7		0	0
-1	0	0	2	2	6	8		0	0
-1	0	0	2	2	4	6		0	0
-1	0	0	3	3	8	7		0	0
-1	0	0	3	3	9	8		0	0
-1	0	0	3	3	7	6		0	0
-4	0		6	1	17	7		1	0
-4	0		6	1	18	8		1	0
-4	0		6	1	16	6		1	0
-4	0		7	2	20	7		0	0
-4	0		7	2	21	8		0	0
-4	0		7	2	19	6		0	0
-4	0		8	3	23	7		0	0
-4	0		8	3	24	8		0	0
-4	0		8	3	22	6		0	0
-5	0		9	1	26	7		1	0
-5	0		9	1	27	8		1	0
-5	0		9	1	25	6		1	0
-5	0		10	2	29	7		0	0
-5	0		10	2	30	8		0	0
-5	0		10	2	28	6		0	0
-5	0		11	3	32	7		0	0
-5	0		11	3	33	8		0	0
-5	0		11	3	31	6		0	0
-6	0		12	1	35	7		1	0
-6	0		12	1	36	8		1	0
-6	0		12	1	34	6		1	0
-6	0		13	2	38	7		0	0
-6	0		13	2	39	8		0	0
-6	0		13	2	37	6		0	0
-6	0		14	3	41	7		0	0
-6	0		14	3	42	8		0	0
-6	0		14	3	40	6		0	0
-2	1	1	4	4	11	7		1	0
-2	1	1	4	4	12	8		1	0
-2	1	1	4	4	10	6		1	0
-3	2	2	5	5	14	7		1	0
-3	2	2	5	5	15	8		1	0
-3	2	2	5	5	13	6		1	0
-7	6	6	15	6	43	1		1	0
-8	7	7	16	7	44	3		1	0
-8	7	7	17	8	45	3		0	0
-8	7	7	18	9	46	3		0	0
-8	7	7	19	10	47	3		0	0
-8	7	7	20	11	48	3		0	0
-8	7	7	21	12	49	3		0	0
-8	7	7	22	13	50	3		0	0
-8	7	7	23	14	51	3		0	0
-8	7	7	24	15	52	3		0	0
-9	8	8	25	16	53	3		1	0
-10	8		26	16	54	3		1	0
-*/
 
   let new_ds = []
 
@@ -300,18 +175,17 @@ cccinstante	duplicateof	id	cccactivitinstante	cccactivitretete	cccantemasuratori
 
   console.log('new_ds', new_ds)
 
-  let transf = convertDBAntemasuratori(new_ds)
+  let ds_estimari_pool = convertDBAntemasuratori(new_ds)
 
   //zero out _start_date and _end_date and _cantitate_estimari in pool
-  for (let i = 0; i < transf.length; i++) {
-    let o = transf[i]
+  for (let i = 0; i < ds_estimari_pool.length; i++) {
+    let o = ds_estimari_pool[i]
     o[_cantitate_estimari_anterioare] = 0
     o[_cantitate_estimari] = 0
     o[_start_date] = ''
     o[_end_date] = ''
   }
 
-  let ds_estimari_pool = [...transf]
 
   console.log('ds_estimari_pool', ds_estimari_pool)
 
