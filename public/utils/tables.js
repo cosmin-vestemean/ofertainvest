186985d47f57365c4037e44b72f2d9b123a1b769
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-09-13 13:14:03 +0300

    Refactor tables.js: Add new tables to allTables() function and update hideAllBut() method

--- public/utils/tables.js
+++ public/utils/tables.js
@@ -1,40 +1,64 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const tables = {
+document.addEventListener('DOMContentLoaded', function () {
+  export const tables = {
+    my_table1: {
+      element: document.getElementById('my_table_oferta_initiala'),
+      visible: true
+    },
+    my_table2: {
+      element: document.getElementById('my_table_recipes'),
+      visible: true
+    },
+    my_table3: {
+      element: document.getElementById('my_table_detalii_reteta'),
+      visible: true
+    },
+    my_table4: {
+      element: document.getElementById('my_table_antemasuratori'),
+      visible: true
+    },
     my_table5: {
       element: document.getElementById('my_table_estimari'),
-      visible: true,
+      visible: true
     },
     my_table6: {
       element: document.getElementById('my_table_lista_estimari'),
-      visible: true,
+      visible: true
     },
     my_table7: {
       element: document.getElementById('my_table_lista_planificari'),
-      visible: true,
+      visible: true
     },
     my_table8: {
       element: document.getElementById('my_table_angajati'),
-      visible: true,
+      visible: true
     },
     allTables() {
-      return [this.my_table5, this.my_table6, this.my_table7, this.my_table8];
+      return [
+        this.my_table1,
+        this.my_table2,
+        this.my_table3,
+        this.my_table4,
+        this.my_table5,
+        this.my_table6,
+        this.my_table7,
+        this.my_table8
+      ]
     },
     hideAllBut(tablesExcept) {
-      this.allTables().forEach(function(table) {
+      this.allTables().forEach(function (table) {
         if (!tablesExcept.includes(table)) {
-          table.visible = false;
-          table.element.style.display = 'none';
+          table.visible = false
+          table.element.style.display = 'none'
         } else {
-          table.visible = true;
-          table.element.style.display = 'block';
+          table.visible = true
+          table.element.style.display = 'block'
         }
-      });
+      })
     }
-  };
+  }
 
   // Export the tables object
   window.tables = tables;
-});
+})
 
-// Export tables for use in other modules
-export { tables };
+export default tables;
