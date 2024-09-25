0af17e7280cb5f1ab796bddbded0a047751c1cdc
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-09-25 18:49:47 +0300

    Refactor index.html: Update tab content ID for Estimari and Timeline

--- public/utils/tables.js
+++ public/utils/tables.js
@@ -35,12 +35,12 @@ export const tables = {
     element: document.getElementById('estimari_timeline'),
     visible: true,
   },
-  myTabEstimari: {
-    element: document.getElementById('myTabEstimari'),
+  estimariTab: {
+    element: document.getElementById('estimariTab'),
     visible: true,
   },
   allTables() {
-      return [this.my_table1, this.my_table2, this.my_table3, this.my_table4, this.my_table5, this.my_table6, this.my_table7, this.my_table8, this.estimari_timeline, this.myTabEstimari];
+      return [this.my_table1, this.my_table2, this.my_table3, this.my_table4, this.my_table5, this.my_table6, this.my_table7, this.my_table8, this.estimari_timeline, this.estimariTab];
   },
   hideAllBut(tablesExcept) {
       this.allTables().forEach(function(table) {
