e18943a4af6a7403dd8509f02ce285b2e047433e
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-09-25 18:33:43 +0300

    Refactor listaEstimari.js: Update title2 innerHTML to 'Gantt'

--- public/views/listaEstimari.js
+++ public/views/listaEstimari.js
@@ -363,7 +363,7 @@ order by A.CCCINSTANTE,
       var title1 = document.createElement('h5')
       title1.innerHTML = 'Lista'
       var title2 = document.createElement('h5')
-      title2.innerHTML = 'Timeline'
+      title2.innerHTML = 'Gantt'
     }
 
     return html`${title1}${table}${title2}${timeline}`
