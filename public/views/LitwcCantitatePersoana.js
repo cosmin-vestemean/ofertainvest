43871cf36507651f5517b132f3dc23ac8c0651ac
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-09-29 17:36:44 +0300

    Refactor LitwcCantitatePersoana.js: Update render method to include new html element

--- public/views/LitwcCantitatePersoana.js
+++ public/views/LitwcCantitatePersoana.js
@@ -11,6 +11,8 @@ Header (colspan=3):
 1. Are un checkbox care selecteaza/deselecteaza toate liniile.
 2. Are un plus_icon care adauga o noua linie in tabel.
 3. Are un trash_icon care sterge liniile selectate (vezi ckeckbox).
+
+If no data, return table with a single empty row so that the user can add a new row.
 */
 
 class LitwcCantitatePersoana extends LitElement {
@@ -30,21 +32,129 @@ class LitwcCantitatePersoana extends LitElement {
   }
 
   render() {
-    return html`can you see me?`
+    var table = document.createElement('table')
+    table.classList.add('table')
+    table.classList.add('table-sm')
+    table.classList.add('table-hover')
+    table.classList.add('table-responsive')
+    table.id = 'detalii_cantitate_persoana'
+    table.style.fontSize = 'small'
+
+    var thead = document.createElement('thead')
+    table.appendChild(thead)
+
+    var tr = document.createElement('tr')
+    thead.appendChild(tr)
+
+    var th = document.createElement('th')
+    //checkbox
+    var input = document.createElement('input')
+    input.type = 'checkbox'
+    input.id = 'select_all'
+    th.appendChild(input)
+    tr.appendChild(th)
+
+    //select angajat
+    th = document.createElement('th')
+    th.textContent = 'Angajat'
+    tr.appendChild(th)
+
+    //cantitate
+    th = document.createElement('th')
+    th.textContent = 'Cantitate'
+    tr.appendChild(th)
+
+    //add rows
+    var tbody = document.createElement('tbody')
+    tbody.id = 'tbody_detalii_cantitate_persoana'
+    table.appendChild(tbody)
+
+    if (!this.ds || this.ds.length == 0) {
+      //add a single empty row
+      this.addEmptyRow()
+    } else {
+      //add rows
+      this.ds.forEach(function (o) {
+        let tr = document.createElement('tr')
+        tbody.appendChild(tr)
+      })
+    }
+
+    return html`${table}`
   }
 
   //class methods
-    //add a new row to the table
-    addRow() {}
+  //add a new row to the table
+  addEmptyRow() {
+    let nrTableRow = tbody.rows.length + 1
+    var tbody = document.getElementById('tbody_detalii_cantitate_persoana')
+    var tr = document.createElement('tr')
+    tbody.appendChild(tr)
 
-    //delete selected rows
-    deleteRows() {}
+    //checkbox
+    var td = document.createElement('td')
+    var input = document.createElement('input')
+    input.id = 'checkbox' + nrTableRow
+    input.type = 'checkbox'
+    td.appendChild(input)
+    tr.appendChild(td)
 
-    //select all rows
-    selectAll() {}
+    //select angajat
+    td = document.createElement('td')
+    var select = document.createElement('select')
+    select.id = 'select_angajat' + nrTableRow
+    //add attr data-searchable="true"
+    select.setAttribute('data-searchable', 'true')
+    let select_angajat = new UseBootstrapSelect(select)
+    contextOferta.angajati.forEach(function (o) {
+      select_angajat.addOption(o.PRSN, o.NAME2)
+    })
+    td.appendChild(select)
 
-    //deselect all rows
-    deselectAll() {}
+    //cantitate
+    td = document.createElement('td')
+    var input = document.createElement('input')
+    input.id = 'cantitate' + nrTableRow
+    input.type = 'text'
+    input.size = 5
+    td.appendChild(input)
+    tr.appendChild(td)
+  }
+
+  //delete selected rows
+  deleteRows() {
+    var tbody = document.getElementById('tbody_detalii_cantitate_persoana')
+    var rows = tbody.rows
+    for (var i = rows.length - 1; i >= 0; i--) {
+      var row = rows[i]
+      var checkbox = row.cells[0].firstChild
+      if (checkbox.checked) {
+        tbody.removeChild(row)
+      }
+    }
+  }
+
+  //select all rows
+  selectAll() {
+    var tbody = document.getElementById('tbody_detalii_cantitate_persoana')
+    var rows = tbody.rows
+    for (var i = 0; i < rows.length; i++) {
+      var row = rows[i]
+      var checkbox = row.cells[0].firstChild
+      checkbox.checked = true
+    }
+  }
+
+  //deselect all rows
+  deselectAll() {
+    var tbody = document.getElementById('tbody_detalii_cantitate_persoana')
+    var rows = tbody.rows
+    for (var i = 0; i < rows.length; i++) {
+      var row = rows[i]
+      var checkbox = row.cells[0].firstChild
+      checkbox.checked = false
+    }
+  }
 }
 
 export { LitwcCantitatePersoana }
