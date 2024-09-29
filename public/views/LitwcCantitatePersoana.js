c837e98cd0c60662f4df441078d0fed459f5116f
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-09-29 17:45:25 +0300

    Refactor LitwcCantitatePersoana.js: Update render method to include new html element and add getTotalCantitate method

--- public/views/LitwcCantitatePersoana.js
+++ public/views/LitwcCantitatePersoana.js
@@ -17,152 +17,142 @@ If no data, return table with a single empty row so that the user can add a new 
 
 class LitwcCantitatePersoana extends LitElement {
   static properties = {
-    ds: { type: Array } //[{CCCESTIMARIPERS: 1, PRSN: 12, NAME2: 'Popescu Ion', CANTITATE: 10}]
+    ds: { type: Array } // [{CCCESTIMARIPERS: 1, PRSN: 12, NAME2: 'Popescu Ion', CANTITATE: 10}]
   }
 
-  //lit methods
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this.table = this.createTable()
   }
 
   connectedCallback() {
     super.connectedCallback()
   }
 
-  render() {
-    var table = document.createElement('table')
-    table.classList.add('table')
-    table.classList.add('table-sm')
-    table.classList.add('table-hover')
-    table.classList.add('table-responsive')
+  createTable() {
+    const table = document.createElement('table')
+    table.classList.add('table', 'table-sm', 'table-hover', 'table-responsive')
     table.id = 'detalii_cantitate_persoana'
     table.style.fontSize = 'small'
 
-    var thead = document.createElement('thead')
+    const thead = document.createElement('thead')
     table.appendChild(thead)
 
-    var tr = document.createElement('tr')
+    const tr = document.createElement('tr')
     thead.appendChild(tr)
 
-    var th = document.createElement('th')
-    //checkbox
-    var input = document.createElement('input')
+    let th = document.createElement('th')
+    const input = document.createElement('input')
     input.type = 'checkbox'
     input.id = 'select_all'
     th.appendChild(input)
     tr.appendChild(th)
 
-    //select angajat
     th = document.createElement('th')
     th.textContent = 'Angajat'
     tr.appendChild(th)
 
-    //cantitate
     th = document.createElement('th')
     th.textContent = 'Cantitate'
     tr.appendChild(th)
 
-    //add rows
-    var tbody = document.createElement('tbody')
+    const tbody = document.createElement('tbody')
     tbody.id = 'tbody_detalii_cantitate_persoana'
     table.appendChild(tbody)
 
-    if (!this.ds || this.ds.length == 0) {
-      //add a single empty row
+    return table
+  }
+
+  render() {
+    const tbody = this.table.querySelector('#tbody_detalii_cantitate_persoana')
+    tbody.innerHTML = '' // Clear existing rows
+
+    if (!this.ds || this.ds.length === 0) {
       this.addEmptyRow()
     } else {
-      //add rows
-      this.ds.forEach(function (o) {
-        let tr = document.createElement('tr')
+      this.ds.forEach((o) => {
+        const tr = document.createElement('tr')
         tbody.appendChild(tr)
       })
     }
 
-    return html`${table}`
+    return html`${this.table}`
   }
 
-  //class methods
-  //add a new row to the table
   addEmptyRow() {
-    var tbody = document.getElementById('tbody_detalii_cantitate_persoana')
-    let nrTableRow = tbody.rows.length + 1
-    var tr = document.createElement('tr')
+    const tbody = this.shadowRoot.getElementById('tbody_detalii_cantitate_persoana')
+    const nrTableRow = tbody.rows.length + 1
+    const tr = document.createElement('tr')
     tbody.appendChild(tr)
 
-    //checkbox
-    var td = document.createElement('td')
-    var input = document.createElement('input')
+    let td = document.createElement('td')
+    const input = document.createElement('input')
     input.id = 'checkbox' + nrTableRow
     input.type = 'checkbox'
     td.appendChild(input)
     tr.appendChild(td)
 
-    //select angajat
     td = document.createElement('td')
-    var select = document.createElement('select')
+    const select = document.createElement('select')
     select.id = 'select_angajat' + nrTableRow
-    //add attr data-searchable="true"
     select.setAttribute('data-searchable', 'true')
-    let select_angajat = new UseBootstrapSelect(select)
-    contextOferta.angajati.forEach(function (o) {
+    const select_angajat = new UseBootstrapSelect(select)
+    contextOferta.angajati.forEach((o) => {
       select_angajat.addOption(o.PRSN, o.NAME2)
     })
     td.appendChild(select)
+    tr.appendChild(td)
 
-    //cantitate
     td = document.createElement('td')
-    var input = document.createElement('input')
-    input.id = 'cantitate' + nrTableRow
-    input.type = 'text'
-    input.size = 5
-    td.appendChild(input)
+    const inputCantitate = document.createElement('input')
+    inputCantitate.id = 'cantitate' + nrTableRow
+    inputCantitate.type = 'text'
+    inputCantitate.size = 5
+    td.appendChild(inputCantitate)
     tr.appendChild(td)
   }
 
-  //delete selected rows
   deleteRows() {
-    var tbody = document.getElementById('tbody_detalii_cantitate_persoana')
-    var rows = tbody.rows
-    for (var i = rows.length - 1; i >= 0; i--) {
-      var row = rows[i]
-      var checkbox = row.cells[0].firstChild
+    const tbody = this.shadowRoot.getElementById('tbody_detalii_cantitate_persoana')
+    const rows = tbody.rows
+    for (let i = rows.length - 1; i >= 0; i--) {
+      const row = rows[i]
+      const checkbox = row.cells[0].firstChild
       if (checkbox.checked) {
         tbody.removeChild(row)
       }
     }
   }
 
-  //select all rows
   selectAll() {
-    var tbody = document.getElementById('tbody_detalii_cantitate_persoana')
-    var rows = tbody.rows
-    for (var i = 0; i < rows.length; i++) {
-      var row = rows[i]
-      var checkbox = row.cells[0].firstChild
+    const tbody = this.shadowRoot.getElementById('tbody_detalii_cantitate_persoana')
+    const rows = tbody.rows
+    for (let i = 0; i < rows.length; i++) {
+      const row = rows[i]
+      const checkbox = row.cells[0].firstChild
       checkbox.checked = true
     }
   }
 
-  //deselect all rows
   deselectAll() {
-    var tbody = document.getElementById('tbody_detalii_cantitate_persoana')
-    var rows = tbody.rows
-    for (var i = 0; i < rows.length; i++) {
-      var row = rows[i]
-      var checkbox = row.cells[0].firstChild
+    const tbody = this.shadowRoot.getElementById('tbody_detalii_cantitate_persoana')
+    const rows = tbody.rows
+    for (let i = 0; i < rows.length; i++) {
+      const row = rows[i]
+      const checkbox = row.cells[0].firstChild
       checkbox.checked = false
     }
   }
 
   getTotalCantitate() {
-    var tbody = document.getElementById('tbody_detalii_cantitate_persoana')
-    var rows = tbody.rows
-    var total = 0
-    for (var i = 0; i < rows.length; i++) {
-      var row = rows[i]
-      var cantitate = row.cells[2].firstChild
+    const tbody = this.shadowRoot.getElementById('tbody_detalii_cantitate_persoana')
+    const rows = tbody.rows
+    let total = 0
+    for (let i = 0; i < rows.length; i++) {
+      const row = rows[i]
+      const cantitate = row.cells[2].firstChild
       total += parseFloat(cantitate.value)
     }
     return total
