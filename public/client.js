c56c8b2d2a9a68df80b61303252255a882e7ed4d
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-09-29 18:00:42 +0300

    Update client.js to include use-bootstrap-select CSS

--- public/client.js
+++ public/client.js
@@ -609,7 +609,8 @@ template.innerHTML = `
   src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
   crossorigin="anonymous">
-</script>`
+</script>
+<script src="https://cdn.jsdelivr.net/npm/use-bootstrap-select@2.2.0/dist/use-bootstrap-select.min.js"></script>`
 
 // 1. load excel file by file chooser xlsx.js
 export function loadDataFromFile(evt) {
