3c847a6f9a91ab53e24c4c082b6e9851fd5631b3
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-10-08 16:55:26 +0300

    Refactor configuration files and remove unused code

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,3 @@
-export {}
-
-import { user } from './users/users.js'
-
 export const services = (app) => {
-  app.configure(user)
-
-  app.configure(user)
-
   // All services will be registered here
 }
