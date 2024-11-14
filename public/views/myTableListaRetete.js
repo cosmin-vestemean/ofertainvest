9d5d8053111d32edc4dc3b9eeeade93024bca074
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-11-14 17:18:31 +0200

    Refactor MyTableListaRetete to streamline rendering logic by removing unnecessary subitem handling and enhancing data structure usage

--- public/views/myTableListaRetete.js
+++ public/views/myTableListaRetete.js
@@ -40,50 +40,17 @@ class MyTableListaRetete extends LitElement {
       noDataMessage.textContent = 'No data'
       return html`${noDataMessage}`
     } else {
-      // Add main div container
+      //add main div container
       let container = document.createElement('div')
       container.className = 'container-fluid'
       this.data.forEach((reteta) => {
         reteta.reteta.forEach((activitate) => {
           let articol = activitate.object
-          // Div class row with divs class col for each column from recipeDisplayMask, if articol key exists in recipeDisplayMask has visible property set to true
-          let row = document.createElement('div')
-          row.className = 'row'
-          for (var key in recipeDisplayMask) {
-            if (Object.prototype.hasOwnProperty.call(articol, key)) {
-              let column = recipeDisplayMask[key]
-              if (articol[column.value] && column.visible) {
-                let col = document.createElement('div')
-                col.className = 'col'
-                col.innerHTML = articol[column.label]
-                row.appendChild(col)
-                console.log(articol[column.value], articol[column.label])
-              }
-            }
-          }
-          container.appendChild(row)
-
-          let subarticole = activitate.children
-          subarticole.forEach((subarticol) => {
-            let subarticolObject = subarticol.object
-            let subrow = document.createElement('div')
-            subrow.className = 'row'
-            for (var key in recipeDisplayMask) {
-              if (Object.prototype.hasOwnProperty.call(subarticol, key)) {
-                let column = recipeDisplayMask[key]
-                if (subarticolObject[column.value] && column.visible) {
-                  let subcol = document.createElement('div')
-                  subcol.className = 'col'
-                  subcol.innerHTML = subarticolObject[column.label]
-                  subrow.appendChild(subcol)
-                  console.log(subarticolObject[column.value], subarticolObject[column.label])
-                }
-              }
-            }
-            container.appendChild(subrow)
-          })
+          let subarticole = activitate.children.map((subarticol) => subarticol.object)
+          console.log('articol', articol, 'subarticole', subarticole)
         })
       })
+
       return html`${container}`
     }
   }
