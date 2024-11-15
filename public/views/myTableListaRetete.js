22a9f8b277bfbdf1ceddd43a05ddf60508ae236e
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-11-15 14:24:02 +0200

    Remove commented-out Bootstrap and use-bootstrap-select script tags for cleaner code

--- public/views/myTableListaRetete.js
+++ public/views/myTableListaRetete.js
@@ -32,10 +32,6 @@ class MyTableListaRetete extends LitElement {
   }
 
   script1 = () => {
-  /*
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/use-bootstrap-select@2.2.0/dist/use-bootstrap-select.min.js"></script>
-  */
     let script = document.createElement('script')
     script.onload = this.onLoad.bind(this)
     script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
@@ -52,7 +48,7 @@ class MyTableListaRetete extends LitElement {
   }
 
   onLoad() {
-    alert('loaded')
+    console.log('Script loaded')
   }
 
   render() {
