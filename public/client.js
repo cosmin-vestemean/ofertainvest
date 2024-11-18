691a746790db895a2d96a799bceabecc6b14804b
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-11-18 17:57:25 +0200

    Update Bootstrap version in index.html to 5.2.3

--- public/client.js
+++ public/client.js
@@ -578,7 +578,7 @@ let themeLink =
     ? ''
     : `<link id="theme_link" rel="stylesheet" href="./css/${selectedTheme}.css">`
 template.innerHTML = `
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css"/>${themeLink}
 <style>
 @media screen and (max-width: 800px) {
