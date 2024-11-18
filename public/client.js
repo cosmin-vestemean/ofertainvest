c687693bd212350b3abe2a77219127491d6da02a
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-11-18 17:37:04 +0200

    Remove Bootstrap Select stylesheet link from client.js

--- public/client.js
+++ public/client.js
@@ -580,7 +580,6 @@ let themeLink =
 template.innerHTML = `
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css"/>${themeLink}
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/use-bootstrap-select@2.2.0/dist/use-bootstrap-select.min.css"/>
 <style>
 @media screen and (max-width: 800px) {
         /*responsive table: 1. every tr is displayed like a card with margin 2. every td will stacked vertically to be scrolled*/
