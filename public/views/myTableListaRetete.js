afcae523af6387e0713841053b641781c3a3f4df
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-12-03 23:53:27 +0200

    Update tbody class in MyTableListaRetete component for improved styling

--- public/views/myTableListaRetete.js
+++ public/views/myTableListaRetete.js
@@ -92,7 +92,7 @@ class MyTableListaRetete extends LitElement {
             </thead>
             ${this.articole.map(
               (item, index) =>
-                html`<tbody class="cardReteta shadow-sm">
+                html`<tbody class="cardReteta shadow">
                   ${this.renderArticleRow(
                     item,
                     index,
