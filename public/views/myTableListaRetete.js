19e3f5d309ded3c056ed172421ca0f3e2c40d6a1
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-12-03 20:02:15 +0200

    Make popover borderless in MyTableListaRetete

--- public/views/myTableListaRetete.js
+++ public/views/myTableListaRetete.js
@@ -204,6 +204,8 @@ class MyTableListaRetete extends LitElement {
       const popover = document.createElement('div');
       popover.className = 'popover';
       popover.style.position = 'absolute';
+      //make div borderless
+      popover.style.border = 'none';
       popover.innerHTML = popoverContent;
       this.appendChild(popover);
       const rect = tr.getBoundingClientRect();
