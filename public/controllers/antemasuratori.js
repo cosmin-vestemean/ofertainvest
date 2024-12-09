bd4d71a4c4d7ec4dcdd3ab237e91db94969cb8ec
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-12-09 16:22:54 +0200

    Update convertDBAntemasuratori to wrap items in an object for consistent data structure

--- public/controllers/antemasuratori.js
+++ public/controllers/antemasuratori.js
@@ -260,7 +260,7 @@ export async function convertDBAntemasuratori(antemasuratori) {
       if (!acc[key]) {
         acc[key] = []
       }
-      acc[key].push(item)
+      acc[key].push({ object: item || {} })
       return acc
     }, {})
 
