3d02a45b7569275bb90bbbfa27605a0dfb9d9eb1
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-12-17 12:07:05 +0200

    Refactor form elements to use Bootstrap's form-floating and improve input styling

--- public/views/UI1.js
+++ public/views/UI1.js
@@ -110,18 +110,18 @@ class UI1 extends LitElement {
       .map((key) => {
         if (this.mainMask[key].filter === 'filter') {
           return `
-            <div class="mb-3">
+            <div class="form-floating mb-3">
               <label for="${key}" class="form-label">${this.mainMask[key].label}</label>
-              <select class="form-select" id="${key}" name="${key}">
+              <select class="form-select form-select-sm" id="${key}" name="${key}">
                 ${this.getFilterOptions(key)}
               </select>
             </div>
           `
         } else {
           return `
-            <div class="mb-3">
+            <div class="form-floating mb-3">
               <label for="${key}" class="form-label">${this.mainMask[key].label}</label>
-              <input type="text" class="form-control" id="${key}" name="${key}">
+              <input type="text" class="form-control form-control-sm" id="${key}" name="${key}" placeholder="${this.mainMask[key].label}">
             </div>
           `
         }
