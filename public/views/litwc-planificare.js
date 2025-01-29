44506ff6cad0227afd9fffb62c02e3e788ddff8d
Cosmin Veștemean <cosmin.ve@gmail.com>
2025-01-29 15:31:44 +0200

    Add console warning for no valid planning lines in Planificare class

--- public/views/litwc-planificare.js
+++ public/views/litwc-planificare.js
@@ -58,6 +58,7 @@ export class Planificare extends UI1 {
 
     if (lines.length === 0) {
       alert('Nu am gasit planificari valide')
+      console.warn('No valid planificari found')
       this.restorehtmlElement(htmlElement, originalContent)
       return
     }
