d385f26f08d5b28e95dadaa1a35603d44cdb79a8
Cosmin Veștemean <cosmin.ve@gmail.com>
2025-01-30 17:50:08 +0200

    Add logging for loaded employees in init function

--- public/utils/init.js
+++ public/utils/init.js
@@ -687,6 +687,7 @@ export async function init() {
   //load lisa angajati
   try {
     contextOferta.angajati = await employeesService.loadEmployees()
+    console.log('contextOferta.angajati', contextOferta.angajati)
   } catch (error) {
     console.error('Failed to load employees:', error)
     contextOferta.angajati = []
